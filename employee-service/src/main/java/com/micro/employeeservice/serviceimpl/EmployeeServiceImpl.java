07df3e207d8d69a039a3e140beeac8c4ee9d7507
jagadeshwar <personallaptop@gmail.com>
2024-11-19 12:47:45 +0530

    circuit breaker changes

--- employee-service/src/main/java/com/micro/employeeservice/serviceimpl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/micro/employeeservice/serviceimpl/EmployeeServiceImpl.java
@@ -1,5 +1,8 @@
 package com.micro.employeeservice.serviceimpl;
 
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,11 +19,15 @@ import com.micro.employeeservice.mapper.EmployeeMapper;
 import com.micro.employeeservice.repository.EmployeeRepository;
 import com.micro.employeeservice.service.EmployeeService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
 	private EmployeeRepository employeeRepository;
 
@@ -38,9 +45,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 		return EmployeeMapper.MAPPER.mapToEmployeeDto(savedEmployee);
 	}
 
+//	@CircuitBreaker(name = "EMPLOYEE-SERVICE", fallbackMethod = "getDefaultMethod")
+	@Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultMethod")
 	@Override
 	public APIResponseDto getEmployeeById(Long employeeId) {
 		// TODO Auto-generated method stub
+		
+		LOGGER.info("inside of getEmployeeById()");
 		Employee employee = employeeRepository.findById(employeeId).orElse(null);
 		EmployeeDto employeeDto;
 		if (employee != null) {
@@ -67,6 +78,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 		apiResponse.setDepartmentDto(departmentDto);
 
 		return apiResponse;
+	}
+	
+	public APIResponseDto getDefaultMethod(Long employeeId, Exception exception) {
+		
+		LOGGER.info("inside of getDefaultMethod()");
+		
+		EmployeeDto employeeDto = new EmployeeDto(1L, "firstName", "lastName", "defaultName@gmail.com", "QWERTY");
+		DepartmentDto departmentDto = new DepartmentDto(1L, "defaultName", "default description", "QWERTY");
+
+		APIResponseDto apiResponse = new APIResponseDto();
+		apiResponse.setEmployeeDto(employeeDto);
+		apiResponse.setDepartmentDto(departmentDto);
+
+		return apiResponse;
+		
 	}
 
 }
